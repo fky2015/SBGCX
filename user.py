3755bfe663c25a6bc0c1259f700e2ee3c902c402
KaiYu Feng <loveress01@outlook.com>
2018-01-12 17:51:24 +0800

    Update user.py

--- user.py
+++ user.py
@@ -5,10 +5,20 @@ import sbgc as sb
 
 
 uname = "1120171224"
-upass = "*********"
+upass = "BiT9182736450"
 
 # 把想抢的课的id放进去
-the_list = []
+the_list = set({201720182002231,201720182002251,
+201720182002410,201720182001822,201720182001819,
+201720182001820,201720182001823,201720182001826,201720182001426,
+201720182001430,201720182001429,201720182001433,201720182002261,
+201720182002259,201720182002266,201720182001829,201720182000663,
+201720182002350,201720182000128,201720182000127,201720182002200,
+201720182002204,201720182002205,201720182000680,201720182000771,
+201720182000659,201720182000654,201720182000673,201720182000669,
+201720182002289,201720182002296,201720182001821,201720182002248,
+201720182002409,201720182001378,201720182000120,
+201720182002238,201720182002250,201720182002228})
 total_times = 100
 user = sb.SBGC(uname, upass)
 sleep_time = 1  # seconds
@@ -52,21 +62,24 @@ def process_response_json(jsondict: dict, boolean: bool)->int:
         return 2
 
 
-def grab_all_courses()->dict:
+def grab_all_courses(user: sb.SBGC, filters: bool)->dict:
     # TODO 可以再增加一些filter？
     all_list = []
-    all_list.extend(user.get_courses('t'))
-    all_list.extend(user.get_courses('g'))
-    all_list.extend(user.get_courses('x'))
+    all_list.extend(user.get_courses('t', filters))
+    all_list.extend(user.get_courses('g', filters))
+    all_list.extend(user.get_courses('x', filters))
 
     # 是不是应该再筛选一下？
     return all_list
 
 
-def grab_course(the_list, total_times=100, sleep_time=1):
+def grab_course(the_list: set, total_times: int= 100, sleep_time: int=1, if_mamual: bool =1):
     "抢课，不多说"
-    for i in range(total_times):
-        print(f"{i} in {total_times}")
+    
+    #total_times并没有卵用
+    #for i in range(total_times):
+    while True:
+        #print(f"{i} in {total_times}")
         for course_id in the_list:
 
             # 如果成功
@@ -74,11 +87,20 @@ def grab_course(the_list, total_times=100, sleep_time=1):
                 print(f'已经成功选课{course_id}')
 
                 # 将不能选的课筛选走 TODO
-                for all_id in the_list:
-                    if user.choose_a_course(all_id) == 0:
-                        the_list.remove(all_id)
-                        print(f"因为冲突，已经移除对{all_id}的抢课")
+                # for all_id in the_list:
+                #     if user.choose_a_course(all_id) == 0:
+                #         the_list.remove(all_id)
+                #         print(f"因为冲突，已经移除对{all_id}的抢课")
 
+                # 如果是手动输入模式就直接退出
+                # 原因是：手动模式要避免对get_course函数的依赖
+                if if_mamual:
+                    the_list.remove(course_id)
+                else:
+                    intercourse = grab_all_courses(user, True)
+                    the_list &= intercourse
+
+                    break
         # 沉睡
         time.sleep(sleep_time)
         return
@@ -86,6 +108,34 @@ def grab_course(the_list, total_times=100, sleep_time=1):
 
 
 # 测试代码start
-the_list = list(create_course_id([1234, 3333, 2222, 8888, 7476, 2342]))
-print(len(grab_all_courses()))
-#——————end
+
+# 选出所有的可以抢的课程
+def auto_GC(total_times=20000, sleep_time=1):
+    the_list = set(grab_all_courses(user, 1))
+    grab_course(the_list, total_times, sleep_time)
+
+
+def manual_GC(total_times=20000, sleep_time=1):
+    the_list = set({201720182002231,201720182002251,
+201720182002410,201720182001822,201720182001819,
+201720182001820,201720182001823,201720182001826,201720182001426,
+201720182001430,201720182001429,201720182001433,201720182002261,
+201720182002259,201720182002266,201720182001829,201720182000663,
+201720182002350,201720182000128,201720182000127,201720182002200,
+201720182002204,201720182002205,201720182000680,201720182000771,
+201720182000659,201720182000654,201720182000673,201720182000669,
+201720182002289,201720182002296,201720182001821,201720182002248,
+201720182002409,201720182001378,201720182000120,
+201720182002238,201720182002250,201720182002228})
+    grab_course(the_list, total_times, sleep_time)
+    #——————end
+"""
+问题1：
+没抢到课的响应头中的json是什么？
+因为要针对这个进行分析
+
+问题2：
+如果
+"""
+if __name__ == "__main__":
+    manual_GC(None,sleep_time)
